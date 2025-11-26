159a1b5264a4358265411bfe659556a163e7b427
Parvaneh esmaili <parvanehun@gmail.com>
2025-11-26 05:53:41 -0800

    FEAT: Add FlappyBird Class With Fly And Gravity Logi

--- src/app/_modules/games/_components/flapy-bird/flapy-bird.ts
+++ src/app/_modules/games/_components/flapy-bird/flapy-bird.ts
@@ -2,10 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-flapy-bird',
+  standalone: true,
   imports: [],
   templateUrl: './flapy-bird.html',
-  styleUrl: './flapy-bird.css'
+  styleUrl: './flapy-bird.css',
 })
 export class FlapyBird {
+  positionY: number = 0;
+  gravity: number = 2;
 
+  constructor() {
+    this.positionY = window.innerHeight / 2;
+
+    setInterval(() => {
+      this.fall();
+    }, 30);
+  }
+
+  fly() {
+    this.positionY -= 50;
+    if (this.positionY < 0) {
+      this.positionY = 0;
+    }
+  }
+
+  fall() {
+    this.positionY += this.gravity;
+
+    const ground = window.innerHeight - 50;
+    if (this.positionY > ground) {
+      this.positionY = ground;
+    }
+  }
 }
